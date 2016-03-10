32c6ebcc2ead177f8c829d23366e3ea7ca4ff8d1
Jip Moors <jip@yoast.com>
2016-03-10 12:20:19 +0100

    Initial commit.

---
+++ assets/js/drug.js
@@ -0,0 +1,42 @@
+(function ($) {
+
+    $(document).ready(function () {
+        showData(location.href.split('?')[1].split('=')[1]);
+
+        $('#back').click(gotoIndex);
+    });
+
+    function showData(drug) {
+        $.getJSON('data/drugs/' + drug + '.json', displayData)
+            .error(gotoIndex);
+    }
+
+    function gotoIndex() {
+        location.href = 'index.html';
+    }
+
+    function displayData(data) {
+        var $main = $('main');
+        $main.append( $('<h1>').html(data.name) );
+        $main.append( $('<span>').html( data.details.induction ) );
+        $main.append( $('<span>').html( data.details.duration ) );
+        $main.append( $('<span>').html( data.details.category.join(', ') ) );
+
+        if ( data.details.information ) {
+
+            var $information = $('<section>');
+
+            console.log(data.details.information);
+
+            for( var subject in data.details.information ) {
+                $information.append( $('<h2>').html( subject ) );
+                for ( var paragraph in data.details.information[subject] ) {
+                    $information.append( $('<p>').html( data.details.information[subject][paragraph] ) );
+                }
+            }
+
+            $main.append($information);
+        }
+    }
+
+})(jQuery);
---
+++ assets/js/listing.js
@@ -0,0 +1,59 @@
+(function ($) {
+
+    $(document).ready(function () {
+        updateList();
+        $('#search').on('change keyup blur', search);
+    });
+
+    function updateList() {
+        $.getJSON('data/index.json', parseListing);
+    }
+
+    function parseListing(listing) {
+        for (var index in listing) {
+            addToList(index, listing[index]);
+        }
+    }
+
+    function addToList(title, data) {
+        var $html = $('<article class="drug">');
+        $html.data('drug', title);
+        $html.append($('<h1>').html(title));
+        $html.append($('<span>').html(data.duration));
+        $html.append($('<span>').html(data.category.join(', ')));
+
+        $html.click(gotoPage);
+
+        $('#list').append($html);
+    }
+
+    function gotoPage() {
+        var $this = $(this);
+
+        location.href = 'drug.html?drug=' + $this.data('drug');
+    }
+
+    function search() {
+        var search = $(this).val();
+
+        $('#list > article').each(function () {
+            var $this = $(this);
+            var found = false;
+
+            // Title search
+            found = ( -1 !== $this.find('h1').html().indexOf(search) );
+
+            // Info search
+            $this.find('span').each(function () {
+                found = found || ( -1 !== $(this).html().indexOf(search) );
+            });
+
+            if (found) {
+                $this.removeClass('hidden');
+            } else {
+                $this.addClass('hidden');
+            }
+        });
+    }
+
+})(jQuery);
