4d3a33a633b82cf9f326d2bc35e59d138ca75b1b
Jip Moors <jip@yoast.com>
2016-03-10 15:22:32 +0100

    generate js tool node tools/generate_index.js

--- tools/generate_index.js
+++ tools/generate_index.js
@@ -1,7 +1,22 @@
 var jsonfile = require('jsonfile');
 var util = require('util');
+var fs = require('fs');
 
-var file = '../www/data/data.json';
-jsonfile.readFile(file, function(err, obj) {
-    console.dir(obj)
-});
+var baseDir = 'www/data/';
+var drugsDir = baseDir + 'drugs/';
+
+var listingFile = baseDir + 'index.json';
+var listing = {};
+
+var files = fs.readdirSync(drugsDir);
+
+for (var file in files) {
+    var obj = jsonfile.readFileSync(drugsDir + files[file]);
+
+    listing[obj.name] = {
+        duration: obj.details.duration,
+        category: obj.details.category
+    };
+}
+
+jsonfile.writeFile(listingFile, listing);
